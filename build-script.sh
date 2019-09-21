1db1e2988202115cbc25d6fbeb9ea966212b0fc0
Danar Widi <widiu7omo@gmail.com>
2019-09-22 02:54:30 +0800

    Change Zip path deploy

--- build-script.sh
+++ build-script.sh
@@ -1,3 +1,3 @@
 ant -Dplatforms.JDK_12.home=$JAVA_HOME jar
-cd ~/build/widiu7omo/SIMRS-Khanza/build
-zip -9 ~/SIMRS-Khanza.zip .
+cd ~/build/widiu7omo/SIMRS-Khanza/
+zip -9 -r ~/SIMRS-Khanza.zip ./dist
