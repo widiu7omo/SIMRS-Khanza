4557d6040216408b0ce5e1d666fd2b556aca9484
Danar Widi <widiu7omo@gmail.com>
2019-09-28 17:10:32 +0800

    add lib to dist

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,6 @@
 ant -Dplatforms.JDK_12.home=$JAVA_HOME jar
 cd ~/build/widiu7omo/SIMRS-Khanza/
+cp -r ./lib ./dist/lib
 zip -9 -r SIMRS-Khanza-Client.zip ./dist/*
 cp -r ./webapps ./dist/webapps
 cp -r ./sik.sql ./dist/new_sik.sql
