e837f0ec333d6023c0ccd549f7ca9a9ae08a3d49
Danar Widi <widiu7omo@gmail.com>
2019-09-28 16:59:27 +0800

    A Part Client and Server Khanza

--- build-script.sh
+++ build-script.sh
@@ -1,3 +1,6 @@
 ant -Dplatforms.JDK_12.home=$JAVA_HOME jar
 cd ~/build/widiu7omo/SIMRS-Khanza/
-zip -9 -r SIMRS-Khanza.zip ./dist
+zip -9 -r SIMRS-Khanza-Client.zip ./dist/*
+cp -r ./webapps ./dist/webapps
+cp -r ./sik.sql ./dist/new_sik.sql
+zip -9 -r SIMRS-Khanza-Server.zip ./dist/*
